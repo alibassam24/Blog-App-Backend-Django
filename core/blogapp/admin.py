ba4bcc6bd67d1ebc9a6a48763860c7546b5664a2
Ali <alibassam063@gmail.com>
2025-07-21 12:21:08 +0500

    second commit

--- core/blogapp/admin.py
+++ core/blogapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from blogapp.models import User, Blog
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Blog)
--- core/blogapp/models.py
+++ core/blogapp/models.py
@@ -1,14 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import  AbstractUser
+
 # Create your models here.
 
-class Users(AbstractUser):
-    pass
 
-class blogs(models.Model):
-    id=models.AutoField(primary_key=True)
-    title=models.CharField(max_length=60)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
-    content=models.CharField(max_length=2000)
-    author_id= models.ForeignKey(Users,on_delete=models.CASCADE,editable=False)
+class User(AbstractUser):
+    #  id=models.AutoField(primary_key=True)
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    bio = models.CharField(max_length=150, blank=True,null=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+
+
+class Blog(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=60)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.CharField(max_length=2000)
+    author_id = models.ForeignKey(
+        User, to_field="id", on_delete=models.CASCADE, editable=False
+    )
