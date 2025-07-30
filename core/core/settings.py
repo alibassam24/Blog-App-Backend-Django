554088d937b91bdb792a08df8062e3b1c602a3f5
Ali Bassam <alibassam063@gmail.com>
2025-07-30 11:55:21 +0500

    connected postgresql from supabase instead of sqlite

--- core/core/settings.py
+++ core/core/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -92,9 +92,13 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": config('DB_HOST'),
+        "NAME":config('DB_NAME'), # from Supabase
+        "USER":config('DB_USER'), # from Supabase
+        "PASSWORD":config('DB_PASSWORD'), #FROM ENV FILES
+        "PORT":config('DB_PORT'),
+        }
 }
 
 
